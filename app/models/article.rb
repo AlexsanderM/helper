b0145bfeff3c37ab0a0a03349f829d29c87498ae
alex <malov.alexsander@yandex.ru>
2016-11-22 22:55:47 +0200

    delet connected object

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ActiveRecord::Base
-  has_many :comments
+  has_many :comments, dependent: :destroy
+  # Если удаляете статью, связанные с ней комментарии также должны быть
+  # удалены, в противном случае они будут просто занимать место в базе данных.
+  # Rails позволяет использовать опцию dependent на связи для достижения этого.
   validates :title, presence: true,
                     length: {minimum: 5}
 
